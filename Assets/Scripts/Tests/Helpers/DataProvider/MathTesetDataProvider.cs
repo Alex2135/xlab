a68f3ae8fcb13d398199923ae54cf446601598a1
Alex2135 <leha3521@gmail.com>
2021-07-29 15:28:20 +0300

    Math king base update

---
+++ Assets/Scripts/Tests/Helpers/DataProvider/MathTesetDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NewQuestionModel;
+
+public class MathTesetDataProvider : MonoBehaviour, IDataSource<MathQuestModel>
+{
+    private MathQuestModel GenerateQuests(int lvl)
+    {
+        MathQuestModel result = new MathQuestModel();
+
+
+
+        return result;
+    }
+
+    public IEnumerable<MathQuestModel> GetQuests(TestWholeStats _test)
+    {
+        IEnumerable<MathQuestModel> result = new List<MathQuestModel>();
+
+
+
+        return result;
+    }
+}
